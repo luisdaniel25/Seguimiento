6a4fce8418773c504f1076c75f53bcb2c03a7c7c
luisdaniel25 <tapiasdaniel44@gmail.com>
2026-02-24 12:28:26 -0500

    Implemente Subir Archivos 24-02-2026

---
+++ storage/framework/views/0c537439f389f0b705b849ac56abd075.php
@@ -0,0 +1,2 @@
+
+<?php echo $__env->make('adminlte::auth.register', array_diff_key(get_defined_vars(), ['__data' => 1, '__path' => 1]))->render(); ?><?php /**PATH C:\xampp\htdocs\Seguimiento\resources\views/auth/register.blade.php ENDPATH**/ ?>
